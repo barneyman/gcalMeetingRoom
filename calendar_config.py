3148c74409c33f832d1aaf8d0b74d69634acf538
Barney Flint <b.flint@sensysgatso.com>
2019-09-26 14:47:11 +1000

    remove keys

--- calendar_config.py
+++ calendar_config.py
@@ -1,8 +1,8 @@
 SCOPE = 'https://www.googleapis.com/auth/calendar'
 USER_AGENT = 'GoogleCalendarDisplay/1.0'
-CLIENT_ID = '1055318363172-tishqq1ig7oc9ouvkeuerj2nek1077or.apps.googleusercontent.com'
-CLIENT_SECRET = 'giZiw_iual1srUxTRHG4c9UF'
-DEVELOPER_KEY = 'AIzaSyBFIeRKR1_iJ9eKMMKQ2ZmLy2mVmj9sRVs'
-CALENDAR = 'AU-Melbourne-Ground-AU - Port Melbourne U2 - Upstairs (20)'
-TIMEZONE = 'Australia/Melbourne'
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+DEVELOPER_KEY = ''
+CALENDAR = ''
+TIMEZONE = ''
 
